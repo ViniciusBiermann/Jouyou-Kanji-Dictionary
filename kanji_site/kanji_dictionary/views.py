27d1894c8112ab1880cbc1b1ca16df4bfa098cb8
Vinicius <viniciusbiermann@hotmail.com>
2022-07-03 21:13:23 -0300

    Added radical info to kanji detail view.

--- kanji_site/kanji_dictionary/views.py
+++ kanji_site/kanji_dictionary/views.py
@@ -17,5 +17,10 @@ class KanjiListView(generic.ListView):
     paginate_by = 50
 
 
+# def KanjiDetailView(request, pk):
+#     found_kanji = Kanji.objects.get(id=pk)
+#     print(f"Kanji info is: {found_kanji.radical}")
+#     return render(request, 'kanji_dictionary/kanji_detail.html')
+
 class KanjiDetailView(generic.DetailView):
     model = Kanji
